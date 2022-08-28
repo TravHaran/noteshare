da4a1ea62bc798186d2358c65b682e5a8b31d20f
TravHaran <travharan@yahoo.ca>
2022-08-28 17:07:13 +0000

    fixed book thumbnmail generator

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -15,15 +15,17 @@ def verify(plain_password, hashed_password):
 def generate_thumbnail(input_file, output_dir):
     output_file_name = os.path.basename(input_file).split('.')[0] + '.jpg'
     output = output_dir + output_file_name
-    cmd = ['convert', 
-        input_file + '[0]',
-        '-background', 'white', 
-        '-alpha', 'background', 
-        '-alpha', 
-        'off',
-        '-compress', 'JPEG',
-        '-quality', '85%',
-        '-gaussian-blur', '0.05', 
-        output]
-    call(cmd)
+    # cmd = ['convert', 
+    #     input_file + '[0]',
+    #     '-background', 'white', 
+    #     '-alpha', 'background', 
+    #     '-alpha', 
+    #     'off',
+    #     '-compress', 'JPEG',
+    #     '-quality', '85%',
+    #     '-gaussian-blur', '0.05', 
+    #     output]
+    # /home/derrick/app/src/backend/files/1b36a093-c02d-4ff0-9979-0f518b0cce9c.pdf[0] -background white -alpha background -alpha off -compress JPEG -quality 85% -gaussian-blur 0.05, /home/derrick/app/src/backend/files/thumbnails/test.png
+    cmd = [f"convert {input_file}[0] -background white -alpha background -alpha off -compress JPEG -quality 85% -gaussian-blur 0.05, {output}"]
+    call(cmd, shell=True)
     return output
